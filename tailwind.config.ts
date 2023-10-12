5477eb6d7a7849686c19776d1f58d222fa697876
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-10-12 15:33:07 +0800

    Update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,128 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        black: {
+          50: "rgba(231, 233, 234, 1)",
+          100: "rgba(222, 0, 0, 1)",
+          200: "rgba(206, 0, 0, 1)",
+          300: "rgba(184, 0, 0, 1)",
+          400: "rgba(170, 0, 0, 1)",
+          500: "rgba(18, 32, 47, 1)",
+          600: "rgba(0, 0, 0, 1)",
+          700: "rgba(0, 0, 0, 1)",
+          800: "rgba(0, 0, 0, 1)",
+          900: "rgba(0, 0, 0, 1)",
+        },
+        secondary: {
+          50: "rgba(231, 233, 234, 1)",
+          100: "rgba(222, 0, 0, 1)",
+          200: "rgba(206, 0, 0, 1)",
+          300: "rgba(184, 0, 0, 1)",
+          400: "rgba(170, 0, 0, 1)",
+          500: "rgba(35, 105, 242, 1)",
+          600: "rgba(0, 0, 0, 1)",
+          700: "rgba(0, 0, 0, 1)",
+          800: "rgba(0, 0, 0, 1)",
+          900: "rgba(0, 0, 0, 1)",
+        },
+        white: {
+          50: "rgba(231, 233, 234, 1)",
+          100: "rgba(222, 0, 0, 1)",
+          200: "rgba(206, 0, 0, 1)",
+          300: "rgba(184, 0, 0, 1)",
+          400: "rgba(170, 0, 0, 1)",
+          500: "rgba(247, 248, 240, 1)",
+          600: "rgba(0, 0, 0, 1)",
+          700: "rgba(0, 0, 0, 1)",
+          800: "rgba(0, 0, 0, 1)",
+          900: "rgba(0, 0, 0, 1)",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
+// import type { Config } from "tailwindcss";
+
+// // Use alpha in colors
+// const config: Config = {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: {
+//           50: "rgba(244, 0, 0, 1)",
+//           100: "rgba(222, 0, 0, 1)",
+//           200: "rgba(206, 0, 0, 1)",
+//           300: "rgba(184, 0, 0, 1)",
+//           400: "rgba(170, 0, 0, 1)",
+//           500: "rgba(149, 0, 0, 1)",
+//           600: "rgba(0, 0, 0, 1)",
+//           700: "rgba(0, 0, 0, 1)",
+//           800: "rgba(0, 0, 0, 1)",
+//           900: "rgba(0, 0, 0, 1)",
+//         },
+//         secondary: {
+//           100: "#D1E5E2",
+//           200: "#D1E5E2",
+//           300: "#D1E5E2",
+//           400: "#D1E5E2",
+//           500: "#D1E5E2",
+//           600: "#D1E5E2",
+//           700: "#D1E5E2",
+//           800: "#D1E5E2",
+//           900: "#D1E5E2",
+//         },
+//         accent: {
+//           100: "#D1E5E2",
+//           200: "#D1E5E2",
+//           300: "#D1E5E2",
+//           400: "#D1E5E2",
+//           500: "#D1E5E2",
+//           600: "#D1E5E2",
+//           700: "#D1E5E2",
+//           800: "#D1E5E2",
+//           900: "#D1E5E2",
+//         },
+//         black: {
+//           50: "rgba(231, 233, 234, 1)",
+//           100: "rgba(222, 0, 0, 1)",
+//           200: "rgba(206, 0, 0, 1)",
+//           300: "rgba(184, 0, 0, 1)",
+//           400: "rgba(170, 0, 0, 1)",
+//           500: "rgba(18, 32, 47, 1)",
+//           600: "rgba(0, 0, 0, 1)",
+//           700: "rgba(0, 0, 0, 1)",
+//           800: "rgba(0, 0, 0, 1)",
+//           900: "rgba(0, 0, 0, 1)",
+//         },
+//         light: {
+//           100: "#D1E5E2",
+//           200: "#D1E5E2",
+//           300: "#D1E5E2",
+//           400: "#D1E5E2",
+//           500: "#D1E5E2",
+//           600: "#D1E5E2",
+//           700: "#D1E5E2",
+//           800: "#D1E5E2",
+//           900: "#D1E5E2",
+//         },
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+// export default config;
