2812da47b41a164cccbd5d702361e72ed56a266a
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-10-13 00:22:17 +0800

    Update DetailsSegment

--- app/page.tsx
+++ app/page.tsx
@@ -7,22 +7,8 @@ import {
   DetailsSegment,
   Footer,
 } from "@/components";
-import { get } from "@/services/api";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const mock = new Array(10).fill("Black Purple");
-  const [communities, setcommunities] = useState([]);
-
-  useEffect(() => {
-    const getCommunities = async () => {
-      const { data } = await get("communities");
-      setcommunities(data.data);
-    };
-
-    getCommunities();
-  }, []);
-
   return (
     <main>
       <Navbar />
@@ -31,30 +17,6 @@ export default function Home() {
       <Seize />
       <DetailsSegment />
       <Footer />
-
-      {/* <Navbar />
-      <Carousel />
-      <MyHub />
-      <SideCarousel title="Meet your community!">
-        {communities.map((e: any) => (
-          <CommunityCard title={e.name} image={e.image} />
-        ))}
-      </SideCarousel>
-      <SideCarousel title="Choose your Favorite Collections!">
-        {mock.map((e: string) => (
-          <CollectionCard />
-        ))}
-      </SideCarousel>
-      <SideCarousel title="Buy Event Tickets!  ">
-        {mock.map((e: string) => (
-          <CollectionCard />
-        ))}
-      </SideCarousel>
-      <SideCarousel title="Get a Shoutout! ">
-        {mock.map((e: string) => (
-          <CollectionCard />
-        ))}
-      </SideCarousel> */}
     </main>
   );
 }
